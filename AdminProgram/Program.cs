c8c51cb5693267fb7e60b3789e1568a1610ef5e7
Utopian-2235690 <2235690@talnet.nl>
2026-03-18 11:04:03 +0100

    main menu class aangemaakt
    De class main menu is aangemaakt.

--- AdminProgram/Program.cs
+++ AdminProgram/Program.cs
@@ -10,50 +10,9 @@ namespace AdminProgram
     {
         static void Main(string[] args)
         {
-            bool validEmail = false;
+            mainMenu MainMenu = new mainMenu();
+            MainMenu.mainProgram();
 
-            User user1 = new User("User", "Example@gmail.com", "Obama 23", "06 5556555", 0);
-            List<User> _userList = new List<User>();
-
-
-            Console.WriteLine("1 = Add user");
-            Console.WriteLine("2 = View user");
-            Console.WriteLine("3 = Remove user");
-            Console.WriteLine("4 = Edit user");
-
-            string mainMenuInput = Console.ReadLine();
-
-
-            if (mainMenuInput == "1")
-            {
-                Console.Clear();
-                Console.WriteLine("Name:");
-                string nameInput = Console.ReadLine();
-                user1.Name = nameInput;
-
-                while (!validEmail)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Email:");
-                    string emailInput = Console.ReadLine();
-                    user1.Email = emailInput;
-
-                    if (emailInput.Contains("@gmail.com"))
-                    {
-                        validEmail = true;
-                    }
-                }
-                    
-                if (validEmail)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Name: " + user1.Name);
-                    Console.WriteLine("Email: " + user1.Email);
-                }
-                
-            }
-            
-            //voor volgende keer, test uit de contain functie uit {COMPLETED}
         }
     }
 }
---
+++ AdminProgram/mainMenu.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminProgram
+{
+    internal class mainMenu
+    {
+        public void mainProgram()
+        {
+            //List<User> _userList = new List<User>();
+
+
+            User user1 = new User("User", "Example@gmail.com", "Obama 23", "06 5556555", 0);
+
+            bool validEmail = false;
+
+            Console.WriteLine("1 = Add user");
+            Console.WriteLine("2 = View user");
+            Console.WriteLine("3 = Remove user");
+            Console.WriteLine("4 = Edit user");
+
+            string mainMenuInput = Console.ReadLine();
+
+
+            if (mainMenuInput == "1")
+            {
+                Console.Clear();
+                Console.WriteLine("Name:");
+                string nameInput = Console.ReadLine();
+                user1.Name = nameInput;
+
+                while (!validEmail)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Email:");
+                    string emailInput = Console.ReadLine();
+                    user1.Email = emailInput;
+
+                    if (emailInput.Contains("@gmail.com"))
+                    {
+                        validEmail = true;
+                    }
+                }
+
+                if (validEmail)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Name: " + user1.Name);
+                    Console.WriteLine("Email: " + user1.Email);
+                }
+
+            }
+        }  
+    }
+}
